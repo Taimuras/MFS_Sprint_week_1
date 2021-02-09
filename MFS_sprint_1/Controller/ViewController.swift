5ed8c443268a251c4ddcc9b170d874bb7c07a038
Taimuras <Avelonadreamer@gmail.com>
2021-02-09 11:10:11 +0600

    first commit

---
+++ MFS_sprint_1/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MFS_sprint_1
+//
+//  Created by tami on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
