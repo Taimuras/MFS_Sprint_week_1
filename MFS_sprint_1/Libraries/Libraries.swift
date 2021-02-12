3bfafad0cb40493489dcbae62a56546638263416
Taimuras <Avelonadreamer@gmail.com>
2021-02-12 14:33:42 +0600

    Libraries

---
+++ MFS_sprint_1/Libraries/Libraries.swift
@@ -0,0 +1,33 @@
+//
+//  Libraries.swift
+//  MFS_sprint_1
+//
+//  Created by tami on 2/12/21.
+//
+
+import Foundation
+
+
+
+
+
+//https://cocoapods.org/pods/Charts
+
+
+
+//https://cocoapods.org/pods/Alamofire
+
+
+
+
+//https://cocoapods.org/pods/IQKeyboardManager
+
+
+
+//https://cocoapods.org/pods/TextFieldCustom
+//https://cocoapods.org/pods/TextFieldEffects
+
+
+
+
+
